7683df39b40353d665fdcf8834cdc2a52ab4b948
Bobby Bol <me@bobbybol.com>
2017-01-05 23:24:31 +0100

    moved files

---
+++ traversy_course/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Hello from App parent</h1>
+        <test></test>
+    </div>
+</template>
+
+<script>
+import Test from './components/test'
+    
+export default {
+    name: 'app',
+    components: {
+        Test
+    }
+}
+</script>
---
+++ traversy_course/src/components/test.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="test">
+        <div v-html="title"></div>
+        <p>{{user.firstName}}</p>
+        <p v-if="showName" v-text="user.firstName"></p>
+        <button v-on:click="greet">Say Greeting</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {
+                title: "<h2>Hello World<h2>",
+                user: {
+                    firstName: 'John',
+                    lastName: 'Doe'
+                },
+                showName: false
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
